2a2830f24d213f0092d9d72b3ae95610bf29180c
Pamela Gerber <75913281+Pamgerber@users.noreply.github.com>
2022-04-18 16:02:31 -0300

    Indo

---
+++ LoginApp/LoginApp/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new LoginFragment());
+        pagerAdapter.addFragmet(new CriarFragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    static class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        void addFragmet(CriarFragment fragment) {
+            fragmentList.add(fragment);
+        }
+    }
+
+    private static class FragmentPagerAdapter {
+        public void addFragmet(LoginFragment loginFragment) {
+        }
+    }
+}
