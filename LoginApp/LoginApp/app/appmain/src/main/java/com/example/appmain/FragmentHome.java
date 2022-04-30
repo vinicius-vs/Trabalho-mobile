18040974dcfd760cae13816dd298c47a4451781e
Vinicius <vinicius_stumpf@edu.univali.br>
2022-04-30 17:40:20 -0300

    salve point!

---
+++ LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/FragmentHome.java
@@ -0,0 +1,14 @@
+package com.example.appmain;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentHome extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
