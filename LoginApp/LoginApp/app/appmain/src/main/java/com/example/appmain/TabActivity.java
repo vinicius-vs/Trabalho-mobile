9028a9319ee0eaad1f497d56c7f8fce40967d066
Vinicius <vinicius_stumpf@edu.univali.br>
2022-05-01 13:28:14 -0300

    salve point 05

--- LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/TabActivity.java
+++ LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/TabActivity.java
@@ -37,7 +37,7 @@ public class TabActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Função em construção ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
