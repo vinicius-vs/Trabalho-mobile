b0577e9627c37adb818172e2e601fa39f8dbf9b4
Vinicius <vinicius_stumpf@edu.univali.br>
2022-05-02 19:47:52 -0300

    salve point 2

--- LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/HomeActivity.java
+++ LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/HomeActivity.java
@@ -3,27 +3,41 @@ package com.example.appmain;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import com.example.appmain.databinding.ActivityTabBinding;
+import com.example.appmain.databinding.FragmentHomeBinding;
+import com.example.appmain.ui.main.SectionsPagerAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
+    private FragmentHomeBinding  binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
-    }
 
-    public void publicarPost(View view){
-        Toast.makeText(this, "Função não criada" , Toast.LENGTH_LONG).show();
-    }
 
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        Button button = binding.bPublicar;
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar mySnackbar = Snackbar.make(view, "Em construção", Snackbar.LENGTH_LONG);
+                mySnackbar.show();
+            }
+        });
+    }
 }
