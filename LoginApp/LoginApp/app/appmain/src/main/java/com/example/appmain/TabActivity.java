1913531fcd076ba125ed715a990e46a5f013836c
Vinicius <vinicius_stumpf@edu.univali.br>
2022-05-14 17:38:29 -0300

    adicionado a tela de login como tela inicial

--- LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/TabActivity.java
+++ LoginApp/LoginApp/app/appmain/src/main/java/com/example/appmain/TabActivity.java
@@ -34,7 +34,7 @@ public class TabActivity extends AppCompatActivity {
         tabs.setupWithViewPager(viewPager);
         tabs.getTabAt(0).setIcon(R.drawable.home);
         tabs.getTabAt(1).setIcon(R.drawable.equipe);
-        tabs.getTabAt(2).setIcon(R.drawable.chat);
+        tabs.getTabAt(2).setIcon(R.drawable.chat  );
         tabs.getTabAt(3).setIcon(R.drawable.rank);
         FloatingActionButton fab = binding.fab;
 
